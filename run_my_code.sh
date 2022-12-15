48d2826dcfea3984f8fda93f4070c276a401cc60
liana313 <lianapatel515@gmail.com>
2022-12-15 16:02:34 +0000

    testing search

--- run_my_code.sh
+++ run_my_code.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
+# cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=ON -DBUILD_SHARED_LIBS=ON -B build
+# make -C build -j faiss
+# make -C build demo_new_test
+# ./build/demos/demo_new_test 1000 32 1
+
+
 cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=ON -DBUILD_SHARED_LIBS=ON -B build
 make -C build -j faiss
-make -C build demo_new_test
-./build/demos/demo_new_test 1000 32 1
+make -C build demo_test_search
+./build/demos/demo_test_search 100 32 1
--- run_my_tests.sh
+++ run_my_tests.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
-make -C build demo_new_test
-./build/demos/demo_new_test 1000 32 1
+# make -C build demo_new_test
+# ./build/demos/demo_new_test 1000 32 1
+
+make -C build demo_test_search
+./build/demos/demo_test_search 100 32 1
